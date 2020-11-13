504dc10c80089eeb6a9a1388a78754740966446a
Pedro Costa <pedro.costa@infinitaslearning.com>
2020-11-13 01:20:32 +0000

    cors

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,51 +1,63 @@
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import reducers from './reducers';
-import axios from 'axios';
-import axiosMiddleware from 'redux-axios-middleware';
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
+import axios from 'axios'
+import axiosMiddleware from 'redux-axios-middleware'
 
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-
-
-const production = process.env.NODE_ENV &&
-    process.env.NODE_ENV === "production";
+const production = process.env.NODE_ENV && process.env.NODE_ENV === 'production'
 
 if (!production) {
-    require('dotenv').config();
+  require('dotenv').config()
 }
 
-const restUrl = production ?
-    process.env.PROD_RESTURL :
-    process.env.JSONSERVER_RESTURL;
+const restUrl = production
+  ? process.env.PROD_RESTURL
+  : process.env.JSONSERVER_RESTURL
 
 let middleware = [
-    thunk,
-    axiosMiddleware(axios.create({baseURL:restUrl}))
-];
+  thunk,
+  axiosMiddleware(
+    axios.create({
+      baseURL: restUrl,
+      crossdomain: true,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+      }
+    })
+  )
+]
 
 if (!production) {
-    middleware.push(require('redux-immutable-state-invariant').default());
-    console.log('added redux-immutable-state-invariant');
+  middleware.push(require('redux-immutable-state-invariant').default())
+  console.log('added redux-immutable-state-invariant')
 }
 
-export default function configureStore(initialState = {}) {
+export default function configureStore (initialState = {}) {
+  const composeEnhancers = composeWithDevTools({
+    // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+  })
 
-    const composeEnhancers = composeWithDevTools({
-        // Specify name here, actionsBlacklist, actionsCreators and other options if needed
-    });
+  //const restUrl = 'http://localhost:4000/rest';
+  console.log('configureStore: ' + restUrl)
+  const client = axios.create({
+    //all axios can be used, shown in axios documentation
+    baseURL: restUrl,
+    crossdomain: true,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+    }
+    //responseType: 'json'
+  })
 
-    //const restUrl = 'http://localhost:4000/rest';
-    //console.log('configureStore: ' + restUrl);
-    const client = axios.create({ //all axios can be used, shown in axios documentation
-        baseURL: restUrl,
-        //responseType: 'json'
-    });
-
-    return createStore(
-        reducers,
-        initialState,
-        composeEnhancers(
-            applyMiddleware(...middleware))
-    );
+  return createStore(
+    reducers,
+    initialState,
+    composeEnhancers(applyMiddleware(...middleware))
+  )
 }
