5e99363695500e43b666d75d186c7e09be0ad83c
Pedro Costa <pedro.costa@infinitaslearning.com>
2020-12-10 12:40:02 +0000

    added snow

--- ClientApp/Components/common/CodeCampMenu.js
+++ ClientApp/Components/common/CodeCampMenu.js
@@ -46,34 +46,34 @@ export default function CodeCampMenu () {
       </div> */}
 
       <ul className='header__menu-list js-menu' style={cssProperties}>
-        <li className='close-button-mobile'>
+        <li className='close-button-mobile snow-bg'>
           <a onClick={setCssVisibility} className='js-close-main-menu'>
             <i className='fa fa-remove' />
           </a>
         </li>
-        <li>
+        <li className="snow-bg">
           <Link to='/' onClick={setCssVisibility}>
             Clientes
           </Link>
         </li>
-        <li>
+        <li  className="snow-bg">
           <Link to='/speakers' onClick={setCssVisibility}>
             Servicos
           </Link>
         </li>
 
-        <li>
+        <li className="snow-bg">
           <Link to='/Home' onClick={setCssVisibility}>
             Produtos
           </Link>
         </li>
-        <li>
+        <li className="snow-bg">
           <Link to='/speakers' onClick={setCssVisibility}>
             Marcacoes
           </Link>
         </li>
 
-        <li className='social-icon'>
+        <li className='social-icon snow-bg'>
           <a href='https://www.facebook.com/Margarida-Arag%C3%A3o-cabeleireirosest%C3%A9tica-100236801356384/'>
             <i className='fa fa-facebook' aria-hidden='true'></i>
           </a>
