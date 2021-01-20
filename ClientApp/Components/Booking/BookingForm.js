479aaf15a6e13d92bc52c4ed218e34ac0cb72378
Pedro Costa <pedro.costa@infinitaslearning.com>
2021-01-20 12:24:18 +0000

    added multiSelectReactDropDown

--- ClientApp/Components/Booking/BookingForm.js
+++ ClientApp/Components/Booking/BookingForm.js
@@ -1,84 +1,67 @@
 import React from 'react'
-import TextInput from '../common/TextInput'
 import DateInput from '../common/DateInput'
+import MultiSelectDrop from '../common/MultiSelectDrop'
 
-
-function getCustomerName (customerList, id) {
+function getCustomer (customerList, id) {
   if (!id) {
-    return ''
+    return []
   }
-  const customer = customerList.filter(x => x.id == id)
-  return customer[0].name
+  return customerList.filter(x => x.id == id)
 }
 
-function getServiceName (serviceList, ids) {
-  const services = serviceList.filter(item => ids.includes(item.id))
-
-  let names = ''
-  services.forEach(element => {
-    names = names + element.name + ', '
-  })
-
-  return names.slice(0, -2)
+function getServices (serviceList, ids) {
+  return serviceList.filter(item => ids.includes(item.id))
 }
 
 function BookingForm (props) {
-  const customerName = getCustomerName(props.customers, props.booking.idcliente)
-  const serviceName = getServiceName(
-    props.services,
-    props.booking.services
-  )
-
+  const customerSelected = getCustomer(props.customers, props.booking.idcliente)
+  const servicesSelected = getServices(props.services, props.booking.services)
   let showHtml
   if (!props.states.editingData) {
     showHtml = <div></div>
   } else {
     showHtml = (
       <div>
-      <DateInput
-        id='start'
-        label='Inicio'
-        name='start'
-        value={props.booking.start}
-        onChange={props.onChange}
-        error={props.errors.start}
-        showDate='1'
-      />
-      <DateInput
-        id='end'
-        label='Fim'
-        name='end'
-        value={props.booking.end}
-        onChange={props.onChange}
-        error={props.errors.end}
-        showDate='0'
-      />
+        <DateInput
+          id='start'
+          label='Inicio'
+          name='start'
+          value={props.booking.start}
+          onChange={props.onChange}
+          error={props.errors.start}
+          showDate='1'
+        />
+        <DateInput
+          id='end'
+          label='Fim'
+          name='end'
+          value={props.booking.end}
+          onChange={props.onChange}
+          error={props.errors.end}
+          showDate='0'
+        />
       </div>
     )
   }
 
   return (
     <form onSubmit={props.onSubmit}>
-      {/* <DateTimePicker
-        // onChange={props.onChange}
-        // value={props.booking.start}
-      /> */}
-      <TextInput
-        id='name'
+      <MultiSelectDrop
+        selectedValue={customerSelected}
+        placeholder={'Selecionar Cliente '}
         label='Cliente'
-        name='name'
-        value={customerName}
-        onChange={props.onChange}
-        error={props.errors.name}
-      />
-      <TextInput
-        id='services'
+        options={props.customers}
+        singleSelect={true}
+        displayValue={'name'}
+      ></MultiSelectDrop>
+      <MultiSelectDrop
+        selectedValue={servicesSelected}
+        placeholder={'Selecionar Servico'}
         label='Servicos'
-        name='services'
-        value={serviceName}
-        onChange={props.onChange}
-        error={props.errors.services}
-      />
+        options={props.services}
+        singleSelect={false}
+        displayValue={'name'}
+      ></MultiSelectDrop>
       {showHtml}
       <button
         className='btn btn-outline-primary customerRec'
