d516b6003c7941231f60ce83e7f787024d0e78e3
Pedro Costa <pedro.costa@infinitaslearning.com>
2022-01-02 22:31:49 +0000

    fix button scroll up

--- ClientApp/Components/common/Footer.js
+++ ClientApp/Components/common/Footer.js
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, {  useState } from 'react'
+
 
 export default function Footer () {
   
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollHeight;
+    if (scrolled > 820){
+      setVisible(true)
+    } 
+    else if (scrolled <= 820){
+      setVisible(false)
+    }
+  };
+
+  const scrollToTop = () =>{
+    window.scrollTo({
+      top: 0, 
+      behavior: 'smooth'
+    });
+  };
+
+  // window.addEventListener('scroll', toggleVisible);
+  window.addEventListener('click', toggleVisible);
 
   return (
     <footer className='footer'>
       <div className='container-main'>
         <div className='footer__menu' />
         <div className='footer__disclaimer'>
-        <a href="#" id="toTopBtn" class="cd-top text-replace js-cd-top cd-top--is-visible cd-top--fade-out" data-abc="true" onClick={
-          ()=>  window.scrollTo({
-            top: 0, 
-            behavior: 'smooth'
-          })}>
+        <a href="#" id="toTopBtn" class="cd-top text-replace js-cd-top cd-top--is-visible cd-top--fade-out" data-abc="true" onClick={scrollToTop} style={{display: visible ? 'inline' : 'none'}}>
         </a>
           <p>Versao: 1</p>
           <p>
